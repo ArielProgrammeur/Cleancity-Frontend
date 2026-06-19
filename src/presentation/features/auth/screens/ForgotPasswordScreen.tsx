89fdb058dc7fe2f5ced4b77fcc076bdecd4990a1
Ariel <ngadeuariel1@gmail.com>
2026-06-19 18:04:42 +0100

    update report_screen

--- src/presentation/features/auth/screens/ForgotPasswordScreen.tsx
+++ src/presentation/features/auth/screens/ForgotPasswordScreen.tsx
@@ -47,7 +47,7 @@ export function ForgotPasswordScreen() {
           </View>
           <Text style={styles.title}>Reset password</Text>
           <Text style={styles.subtitle}>
-            Enter your email address and we'll send you a link to reset your password
+            Enter your email address and we&apos;ll send you a link to reset your password
           </Text>
         </View>
 
